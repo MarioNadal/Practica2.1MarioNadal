2455e544f3b47cf20b233133aaa4978ce9b7cf7a
MarioNadalCasa <220056@iessanalberto.com>
2023-11-24 09:36:27 +0100

    24/11/2023 Finalizada,con corrección del profesor.

--- src/main/java/database/InsertarProfesor.java
+++ src/main/java/database/InsertarProfesor.java
@@ -7,80 +7,85 @@ import java.util.Date;
 
 public class InsertarProfesor {
     public static void insertar() {
-        //TODO VA TODO BIEN PERO NO ME LO AÑADE A LOS PROFESORES, AL COMPROBARLO NO ME SALE EL PROFESOR NUEVO
         String especialidadIntroducida = "", apellidosNombreIntroducido, sexoIntroducido;
         Date fechaNacimientoIntroducido;
         int codProfesorIntroducido, codigoCentroIntroducido = 0;
         boolean validarEspecialidad = false, validarCodCentro = false;
         try (Connection miCon = ConexionBD.conectar()) {
-            Statement stmt = miCon.createStatement();
-            while(!validarEspecialidad){
-                ResultSet rs1 = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
-                System.out.println("ESPECIALIDADES: ");
-                System.out.println("--------------------------------------------------------------------------------------");
-                //Saco por pantalla todas las especialidades que hay
-                while(rs1.next()){
-                    System.out.println(rs1.getString("ESPECIALIDAD"));
-                }
-            ResultSet rs2 = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
-                //Recogo la especialidad que quiere el usuario para el nuevo profesor
-                especialidadIntroducida = Leer.introduceString("Introduce la especialidad del nuevo profesor: ");
-                while(rs2.next()){
-                    if(rs1.getString("ESPECIALIDAD").equals(especialidadIntroducida)) {
-                        validarEspecialidad = true;
+            try {
+                Statement stmt = miCon.createStatement();
+                while (!validarEspecialidad) {
+                    ResultSet rs1 = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
+                    System.out.println("ESPECIALIDADES: ");
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    //Saco por pantalla todas las especialidades que hay
+                    while (rs1.next()) {
+                        System.out.println(rs1.getString("ESPECIALIDAD"));
+                    }
+                    ResultSet rs2 = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
+                    //Recogo la especialidad que quiere el usuario para el nuevo profesor
+                    especialidadIntroducida = Leer.introduceString("Introduce la especialidad del nuevo profesor: ");
+                    while (rs2.next()) {
+                        if (rs1.getString("ESPECIALIDAD").equals(especialidadIntroducida)) {
+                            validarEspecialidad = true;
+                        }
+                    }
+                    if (!validarEspecialidad) {
+                        System.out.println("Introduce una especialidad válida");
                     }
                 }
-                if(!validarEspecialidad){
-                    System.out.println("Introduce una especialidad válida");
-                }
-            }
-            while(!validarCodCentro){
-                ResultSet rs3 = stmt.executeQuery("SELECT COD_CENTRO FROM C1_CENTROS");
-                System.out.println("CÓDIGOS CENTROS");
-                System.out.println("--------------------------------------------------------------------------------------");
-                //Saco por la pantalla todos los códigos de los centros que hay
-                while(rs3.next()){
-                    System.out.println(rs3.getInt("COD_CENTRO"));
-                }
-                //Recogo el código del centro que quiere el usuario para el nuevo profesor
-                System.out.println("--------------------------------------------------------------------------------------");
-                codigoCentroIntroducido = Leer.introduceEntero("Introduce el código del centro del nuevo profesor");
-                ResultSet rs4 = stmt.executeQuery("SELECT COD_CENTRO FROM C1_CENTROS");
-                while(rs4.next()){
-                    if(rs4.getInt("COD_CENTRO") == codigoCentroIntroducido){
-                        validarCodCentro = true;
+                while (!validarCodCentro) {
+                    ResultSet rs3 = stmt.executeQuery("SELECT COD_CENTRO FROM C1_CENTROS");
+                    System.out.println("CÓDIGOS CENTROS");
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    //Saco por la pantalla todos los códigos de los centros que hay
+                    while (rs3.next()) {
+                        System.out.println(rs3.getInt("COD_CENTRO"));
+                    }
+                    //Recogo el código del centro que quiere el usuario para el nuevo profesor
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    codigoCentroIntroducido = Leer.introduceEntero("Introduce el código del centro del nuevo profesor");
+                    ResultSet rs4 = stmt.executeQuery("SELECT COD_CENTRO FROM C1_CENTROS");
+                    while (rs4.next()) {
+                        if (rs4.getInt("COD_CENTRO") == codigoCentroIntroducido) {
+                            validarCodCentro = true;
+                        }
+                    }
+                    if (!validarCodCentro) {
+                        System.out.println("Introduce un código de centro válido");
                     }
                 }
-                if(!validarCodCentro){
-                    System.out.println("Introduce un código de centro válido");
-                }
-            }
-            System.out.println("--------------------------------------------------------------------------------------");
-            //Recogo los datos que faltan para poder crear al profesor
-            codProfesorIntroducido = Leer.introduceEntero("Introduce el código del profesor");
-            apellidosNombreIntroducido = Leer.introduceString("Introduce los apellidos, una coma y el nombre del profesor (Apellido1 Apellido2, Nombre)");
-            fechaNacimientoIntroducido = Leer.introduceDate("Introduce la fecha de nacimiento del usuario (Año-Mes-Día)");
-            do{
-                sexoIntroducido = Leer.introduceLetra("Introduce sexo del profesor(H/M)");
-            }while(!sexoIntroducido.equals("H")&&!sexoIntroducido.equals("M"));
-            PreparedStatement pstmt1 = miCon.prepareStatement("INSERT INTO C1_PROFESORES VALUES (?,?,?,null,?,?,?)");
-            pstmt1.setInt(1,codProfesorIntroducido);
-            pstmt1.setString(2,apellidosNombreIntroducido);
-            pstmt1.setString(3,especialidadIntroducida);
-            pstmt1.setString(4, fechaNacimientoIntroducido.toString());
-            pstmt1.setString(5,sexoIntroducido);
-            pstmt1.setInt(6,codigoCentroIntroducido);
-            //EJECUTO EL PREPARED STATEMENT Y AÑADO EL PROFESOR
-            pstmt1.execute();
-            System.out.println("--------------------------------------------------------------------------------------");
-            System.out.println("Profesor introducido correctamente");
-            System.out.println("--------------------------------------------------------------------------------------");
-            //Comprobación de que se ha añaido el nuevo profesor
+                System.out.println("--------------------------------------------------------------------------------------");
+                //Recogo los datos que faltan para poder crear al profesor
+                codProfesorIntroducido = Leer.introduceEntero("Introduce el código del profesor");
+                apellidosNombreIntroducido = Leer.introduceString("Introduce los apellidos, una coma y el nombre del profesor (Apellido1 Apellido2, Nombre)");
+                fechaNacimientoIntroducido = Leer.introduceDate("Introduce la fecha de nacimiento del usuario (Año-Mes-Día)");
+                do {
+                    sexoIntroducido = Leer.introduceLetra("Introduce sexo del profesor(H/M)");
+                } while (!sexoIntroducido.equals("H") && !sexoIntroducido.equals("M"));
+                PreparedStatement pstmt1 = miCon.prepareStatement("INSERT INTO C1_PROFESORES VALUES (?,?,?,NULL,?,?,?)");
+                pstmt1.setInt(1, codProfesorIntroducido);
+                pstmt1.setString(2, apellidosNombreIntroducido);
+                pstmt1.setString(3, especialidadIntroducida);
+                pstmt1.setString(4, fechaNacimientoIntroducido.toString());
+                pstmt1.setString(5, sexoIntroducido);
+                pstmt1.setInt(6, codigoCentroIntroducido);
+                //EJECUTO EL PREPARED STATEMENT Y AÑADO EL PROFESOR
+                pstmt1.execute();
+                System.out.println("--------------------------------------------------------------------------------------");
+                System.out.println("Profesor introducido correctamente");
+                System.out.println("--------------------------------------------------------------------------------------");
+                //Comprobación de que se ha añaido el nuevo profesor
             /*ResultSet rs6 = stmt.executeQuery("SELECT NOMBRE_APE FROM C1_PROFESORES");
             while(rs6.next()){
                 System.out.println(rs6.getString("NOMBRE_APE"));
             }
             System.out.println("--------------------------------------------------------------------------------------");*/
+            }catch(SQLIntegrityConstraintViolationException ex){
+                System.out.println("Error en la integración del SQL.");
+            }catch(SQLSyntaxErrorException ex){
+                System.out.println("Error en el lenguaje de SQL");
+            }
         } catch (SQLException ex) {
             System.out.println("Error en la conexión de la base de datos al listar profesores");
         }
--- src/main/java/database/ListarProfesores.java
+++ src/main/java/database/ListarProfesores.java
@@ -6,36 +6,57 @@ import java.sql.*;
 
 public class ListarProfesores {
     public static void listar(){
+        boolean validarEspecialidad = false;
+        String especialidadIntroducida = "";
         try(Connection miCon = ConexionBD.conectar()){
-            Statement stmt = miCon.createStatement();
-            //Recogo de la base de datos todas las especialidades
-            ResultSet resultSet = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
-            System.out.println("ESPECIALIDADES: ");
-            System.out.println("--------------------------------------------------------------------------------------");
-            //Saco por pantalla todas las especialidades que hay
-            while(resultSet.next()){
-             System.out.println(resultSet.getString("ESPECIALIDAD"));
+            try{
+                Statement stmt = miCon.createStatement();
+                ResultSet resultSet2 = null;
+                while(!validarEspecialidad) {
+                    //Recogo de la base de datos todas las especialidades
+                    ResultSet resultSet = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
+                    System.out.println("ESPECIALIDADES: ");
+                    System.out.println("--------------------------------------------------------------------------------------");
+                    //Saco por pantalla todas las especialidades que hay
+                    while (resultSet.next()) {
+                        System.out.println(resultSet.getString("ESPECIALIDAD"));
+                    }
+                    //El usuario introduce la especialidad de la cúal quiere ver los profesores
+                    especialidadIntroducida = Leer.introduceString("Introduce los  os carácteres de la Especialidad: ");
+                    ResultSet resultSet1 = stmt.executeQuery("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD");
+                    while(resultSet1.next()){
+                        if(resultSet1.getString("ESPECIALIDAD").equals(especialidadIntroducida)){
+                            validarEspecialidad = true;
+                        }
+                    }
+                    if(!validarEspecialidad){
+                        System.out.println("Introduce una especialidad válida");
+                    }else{
+                        //Recogo los profesores de la especialidad introducida por el usuario
+                        PreparedStatement pstmt = miCon.prepareStatement("SELECT NOMBRE_APE FROM C1_PROFESORES WHERE ESPECIALIDAD LIKE ?");
+                        pstmt.setString(1, especialidadIntroducida);
+                        resultSet2 = pstmt.executeQuery();
+                    }
+                }
+                //Recogo el nombre de la especialidad para mostrarlo bonito
+                PreparedStatement pstmt1 = miCon.prepareStatement("SELECT NOMBRE_ESPE FROM C1_ESPECIALIDAD WHERE ESPECIALIDAD LIKE ?");
+                pstmt1.setString(1,especialidadIntroducida);
+                ResultSet resultSet3 = pstmt1.executeQuery();
+                System.out.println("--------------------------------------------------------------------------------------");
+                System.out.println("PROFESORES DE LA ESPECIALIDAD: " + resultSet3.getString("NOMBRE_ESPE"));
+                System.out.println("--------------------------------------------------------------------------------------");
+                //Muestro todos los nombres de los profesores de la especialidad seleccionada
+                while(resultSet2.next()){
+                    System.out.println(resultSet2.getString("NOMBRE_APE"));
+                }
+                System.out.println("--------------------------------------------------------------------------------------");
+            }catch(SQLIntegrityConstraintViolationException ex){
+                System.out.println("Error en la integración del SQL.");
+            }catch(SQLSyntaxErrorException ex){
+                System.out.println("Error en el lenguaje de SQL");
             }
-            //El usuario introduce la especialidad de la cúal quiere ver los profesores
-            String especialidadIntroducida = Leer.introduceString("Introduce los  os carácteres de la Especialidad: ");
-            //Recogo los profesores de la especialidad introducida por el usuario
-            PreparedStatement pstmt = miCon.prepareStatement("SELECT NOMBRE_APE FROM C1_PROFESORES WHERE ESPECIALIDAD LIKE ?");
-            pstmt.setString(1,especialidadIntroducida);
-            ResultSet resultSet2 = pstmt.executeQuery();
-            //Recogo el nombre de la especialidad para mostrarlo bonito
-            PreparedStatement pstmt1 = miCon.prepareStatement("SELECT NOMBRE_ESPE FROM C1_ESPECIALIDAD WHERE ESPECIALIDAD LIKE ?");
-            pstmt1.setString(1,especialidadIntroducida);
-            ResultSet resultSet3 = pstmt1.executeQuery();
-            System.out.println("--------------------------------------------------------------------------------------");
-            System.out.println("PROFESORES DE LA ESPECIALIDAD: " + resultSet3.getString("NOMBRE_ESPE"));
-            System.out.println("--------------------------------------------------------------------------------------");
-            //Muestro todos los nombres de los profesores de la especialidad seleccionada
-            while(resultSet2.next()){
-                System.out.println(resultSet2.getString("NOMBRE_APE"));
-            }
-            System.out.println("--------------------------------------------------------------------------------------");
         }catch(SQLException ex){
-            System.out.println("Error en la conexión de la base de datos al listar profesores");
+            System.out.println("Error en la conexión de la base de datos al listar profesores.");
         }
     }
 }
